485315a6c4fcd4da36783e0e71ae3a3a6ea37888
BennettTheTiger <bas9386@g.rit.edu>
2019-04-19 01:33:19 -0400

    Hey arduino

---
+++ arduino/bluetoothBuddy/bluetoothBuddy.ino
@@ -0,0 +1,13 @@
+//Bennett Schoonerman
+//IGME470 Bluetooth Buddy
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
